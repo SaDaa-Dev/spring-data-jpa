c03bbe5ab136bf5fc3ef0773b23e678e1c29e2cf
jbc <qudcks3000@gmail.com>
2022-12-29 14:08:01 +0900

    sync

--- data-jpa/data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
+++ data-jpa/data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
@@ -1,6 +1,7 @@
 package study.datajpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import java.util.Optional;
