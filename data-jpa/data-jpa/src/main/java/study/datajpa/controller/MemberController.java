3710ae39df520c2604541d37e74130ec4105c96b
jbc <qudcks3000@gmail.com>
2022-12-29 16:32:00 +0900

    sync

--- data-jpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -9,16 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import study.datajpa.dto.MemberDto;
+import study.datajpa.dto.MemberSearchCondition;
+import study.datajpa.dto.MemberTeamDto;
 import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberJpaRepository;
+import study.datajpa.repository.MemberQuerydslRepository;
 import study.datajpa.repository.MemberRepository;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberRepository memberRepository;
+    private final MemberQuerydslRepository memberQuerydslRepository;
 
     @GetMapping("/members/{id}")
     public String findMember(@PathVariable("id") Long id){
@@ -34,6 +40,20 @@ public class MemberController {
     @GetMapping("/members")
     public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable){
         return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition){
+        return memberQuerydslRepository.search(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable){
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable){
+        return memberRepository.searchComplex(condition, pageable);
     }
 
 }
--- data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepositoryCustom.java
+++ data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepositoryCustom.java
@@ -1,9 +1,15 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import study.datajpa.dto.MemberSearchCondition;
+import study.datajpa.dto.MemberTeamDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepositoryCustom {
-    List<Member> findMemberCustom();
+    List<MemberTeamDto> search(MemberSearchCondition condition);
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+    Page<MemberTeamDto> searchComplex(MemberSearchCondition condition, Pageable pageable);
 }
